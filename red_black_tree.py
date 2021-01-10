0f36f0b9f8d5a7d296684e68941577a18ffd962d
Phoebe <phoebe.waters.99@gmail.com>
2021-01-10 15:53:35 +0100

    initial commit

---
+++ red_black_tree.py
@@ -0,0 +1,24 @@
+class RedBlackTree:
+    def __init__(self):
+        pass
+
+    def insert(self,value):
+        return
+
+    def remove(self,value):
+        return
+
+    def search(self,value):
+        return bool
+
+    def path(self,value):
+        return
+
+    def min(self):
+        return
+
+    def max(self):
+        return
+
+    def bfs(self):
+        return
